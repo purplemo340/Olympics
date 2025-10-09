0c7a7dc522255bec8f7d2230cb3895de0e39dd8a
Monae <93678860+purplemo340@users.noreply.github.com>
2025-10-08 21:19:08 -0400

    Update interface

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 #from visual import game_info
 games_info=pd.read_csv('results/games.csv')
+ioc=pd.read_csv('results/ioc.csv')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('Flask_Key')
 @app.route('/' , methods=["POST", "GET"])
@@ -16,6 +17,14 @@ def home():
 
 @app.route('/game/<city>' , methods=["POST", "GET"])
 def game(city):
-    return render_template('game.html', city=city)
+     info=pd.read_csv(f'results/{city}.csv')
+     countries=info['country']
+     abv=info['abv']
+     length=len(countries)
+     if request.method=="POST":
+        game=request.form['Cities']
+        print(game)
+        return redirect(url_for('game', city=game))
+     return render_template('game.html', city=city, games=games_info['City-Year'], countries=countries, abv=abv, length=length)
 if __name__ == "__main__":
     app.run(debug=True)
